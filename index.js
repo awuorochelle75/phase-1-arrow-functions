6be09de7ab86975a83efe6acc99a43e26808a7d5
Rochelle Awuor <awuorochelle75@gmail.com>
2025-03-07 17:54:36 +0300

    modifies index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1,num2){
+    return num1 / num2
+}divide(2000,100)
+
+const add=(num1,num2) =>{
+    return num1 + num2;
+};add()
+
+const square=(num) =>{
+    return num * num
+};square()
